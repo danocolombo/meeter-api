5dcd8752290ad81c47acebd559410edd31189416
Dano <dano@dcolombo.com>
2019-01-01 10:48:35 -0500

    continued work to complete adminHosts functionality.

--- hosts/getHosts.php
+++ hosts/getHosts.php
@@ -13,6 +13,7 @@ $db = $database->connect($client);
 
 //first we get the hosts defined in the meeter table
 
+$query = $db->prepare("SELECT Setting From Meeter WHERE Config = 'HostSet'");
 $query->execute();
 $query->bind_result($setting);
 while($query->fetch()){
@@ -24,7 +25,7 @@ $hostID = explode("|", $hostString);
 // then we get all the users that are active and get the associated names
 //now get the people reference for the hostIDs
 $peeps = array();
-$query = $connection->prepare("SELECT ID, FName, LName FROM people WHERE Active = 1");
+$query = $db->prepare("SELECT ID, FName, LName FROM people WHERE Active = 1");
 $query->execute();
 $query->bind_result($id, $fn, $ln);
 while($query->fetch()){
